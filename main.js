4b354d880d33bd29b1a610adfc5316520c43c285
Mohammed jihad <mohammedjihad229@gmail.com>
2023-03-29 15:04:22 +0200

    mid

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ let Kmpercorsi = Km*0.21;
 
 if(Age < 18){
     
-    Kmpercorsi*0.6;
+    Kmpercorsi*0.8;
 
 }else if(Age >= 65){
 
-    Kmpercorsi*0.20;
+    Kmpercorsi*0.6;
 }
 alert("Il prezzo del tuo biglietto è di " + Kmpercorsi.toFixed(2) + " €");
 console.log(`il prezzo del biglietto ${Kmpercorsi} €`)
